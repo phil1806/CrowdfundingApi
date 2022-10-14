d3070c0fe1d2ab14224d3f07342c712bed025269
akwakwak6 <misson_pol@hotmail.fr>
2022-10-14 12:04:22 +0200

    add DAO lib and demo use it

--- CrowdfundingApi/Program.cs
+++ CrowdfundingApi/Program.cs
@@ -1,3 +1,8 @@
+using ADOLibrary;
+using CrowdfundingApi;
+using System;
+using System.Data.SqlClient;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -21,3 +26,43 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+
+/*
+ * DEMO USE ADO library
+*/
+
+Student Converter(SqlDataReader dr) {
+    return new Student() {
+        Id = (int)dr["Id"],
+        LastName = dr["LastName"].ToString(),
+        FirstName = dr["FirstName"].ToString(),
+        BirthDate = dr["BirthDate"] == DBNull.Value ? new DateTime() : (DateTime)dr["BirthDate"],
+        YearResult = (int)dr["YearResult"],
+        SectionId = (int)dr["SectionId"],
+        Active = (bool)dr["Active"]
+    };
+}
+
+Connection connection = new Connection(" db . . . ..  ");
+#region ExecuteNonQuery
+Command cmdNonQuery = new Command("DELETE FROM Student WHERE Id = 29");
+
+Console.WriteLine("Execute Non Query {0}", connection.ExecuteNonQuery(cmdNonQuery));
+#endregion
+
+#region ExecuteReader
+Command cmd = new Command("SELECT * FROM V_Student WHERE YearResult > 10");
+
+IEnumerable<Student> liste = connection.ExecuteReader<Student>(cmd, Converter);
+
+foreach (Student item in liste) {
+    Console.WriteLine("Id : {0} | Nom : {1} | Résultat : {2}", item.Id, item.LastName, item.YearResult);
+}
+#endregion
+
+#region ExecuteScalar
+Command cmd2 = new Command("SELECT LastName FROM Student WHERE Id = 1");
+
+Console.WriteLine("Execute Scalar :  {0}", connection.ExecuteScalar(cmd2));
+#endregion
---
+++ CrowdfundingApi/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace CrowdfundingApi {
+    public class Student {
+        public int Id { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public int YearResult { get; set; }
+        public int SectionId { get; set; }
+        public bool Active { get; set; }
+    }
+}
