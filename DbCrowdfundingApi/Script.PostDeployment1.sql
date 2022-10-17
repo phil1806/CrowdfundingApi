345e38ec0fcfd3ea1b86bb651922c8cd0a6848ef
Philémon Tchatchoua Kamga <p.tchatchouakamga@stag.technofuturtic.education>
2022-10-17 10:09:18 +0200

    Mise en palce de la procedure de recuperation des contributions des Users de type contributions

--- DbCrowdfundingApi/Script.PostDeployment1.sql
+++ DbCrowdfundingApi/Script.PostDeployment1.sql
@@ -1,7 +1,7 @@
 ﻿/*
 Modèle de script de post-déploiement							
 --------------------------------------------------------------------------------------
- Ce fichier contient des instructions SQL qui seront ajoutées au script de compilation.		
+ Ce fichier contient des instructions SQL qui seront ajoutées au script de compilation.		s
  Utilisez la syntaxe SQLCMD pour inclure un fichier dans le script de post-déploiement.			
  Exemple :      :r .\monfichier.sql								
  Utilisez la syntaxe SQLCMD pour référencer une variable dans le script de post-déploiement.		
@@ -9,11 +9,25 @@ Modèle de script de post-déploiement
                SELECT * FROM [$(TableName)]					
 --------------------------------------------------------------------------------------
 */
+
+
 INSERT INTO Roles (TypeRole) 
 VALUES ('Admin'), ('ProjectOwner'), ('Contributeur');
 
 INSERT INTO StatusProjects (TypeStatus) 
 VALUES ('Submit'), ('Accept'), ('Refused'), ('Encours'), ('Finished');
+
+INSERT INTO Users VALUES ('Phil','Phil@gmail.com','1234$','2000-01-01','abdhheyifjfhgfhjd',1,1);
+INSERT INTO Users VALUES ('Will','Will@gmail.com','1234$','2000-01-01','abdhheyifjfhgfhjd',1,3);
+
+INSERT INTO Projects VALUES ('MyFirstProjet','maDescription','MonObjectif','BE 123654','2022-02-03','2022-02-10',1,1);
+INSERT INTO Projects VALUES ('MyFirstProjet','maDescription','MonObjectif','BE 123654','2022-02-03','2022-02-10',2,1);
+
+
+INSERT INTO Contributions  VALUES(12.32,GETDATE(),1,1);
+INSERT INTO Contributions  VALUES(12.32,GETDATE(),2,1);
+
+
 
 
 
---
+++ DbCrowdfundingApi/StoredProcedure/Procedure1.sql
@@ -0,0 +1,22 @@
+﻿CREATE PROCEDURE [dbo].[Procedure1]
+	@id int
+	
+AS
+BEGIN
+ DECLARE @typeDeRole VARCHAR(100);
+	Select @typeDeRole = typeRole
+	FROM roles
+	JOIN Users
+	ON Roles.Id = IdRole
+	WHERE Users.id = @Id ;
+	IF(@typeDeRole like '%Contributeur%')
+		select C.id, C.Montant,C.DateContribution
+		from Contributions as C
+		inner join Users as U
+		on C.IdUser = U.Id
+		where U.id = @Id;
+	ELSE
+	 SELECT 'je ne suis pas Contributeur...';
+END
+
+
