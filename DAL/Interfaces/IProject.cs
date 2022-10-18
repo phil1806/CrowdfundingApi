2d24fba4248248dc0e495f0067fc59ff061b636c
phil <tchatchoua1806@gmail.com>
2022-10-18 05:43:47 +0200

    Mise en place de l'interface IProject

---
+++ DAL/Interfaces/IProject.cs
@@ -0,0 +1,22 @@
+﻿using DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+    public interface IProject
+    {
+        IEnumerable <Project>GetAllProjects();
+        IEnumerable <Project>GetValidProjects();
+        Project GetProjectById(int id);
+        bool CreateProject();
+
+        bool ValidProject(int id);
+
+        bool UpdateProject(int id);
+        bool DeleteProject(int id);
+    }
+}
