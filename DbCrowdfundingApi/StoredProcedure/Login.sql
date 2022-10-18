af67a226b2d1d5ee01707a3597533e9d134be182
akwakwak6 <misson_pol@hotmail.fr>
2022-10-18 11:13:38 +0200

    Login route works ( sql login procedure as well )

--- DbCrowdfundingApi/StoredProcedure/Login.sql
+++ DbCrowdfundingApi/StoredProcedure/Login.sql
@@ -1,22 +1,16 @@
 ﻿CREATE PROCEDURE [dbo].[Login]
-	@email VARCHAR(100),
-	@pwd VARCHAR(100)
+	@email VARCHAR(150),
+	@pwd VARCHAR(50)
 AS
 	BEGIN
-
-		--SELECT Id, Email, NickName FROM [Users] WHERE  Email LIKE @email
-		
-		DECLARE @salt VARCHAR(100)
+		DECLARE @salt VARCHAR(150)
 		SET @salt = (SELECT Salt FROM [Users] WHERE Email LIKE @email)
 		
 		DECLARE @hash VARBINARY(64)
-	--SELECT @hash HASHBYTES('SHA2_512', CONCAT(@salt, @pwd, @salt));
-	--SET @hash = HASHBYTES('SHA2_512', CONCAT(@salt, @pdw, @salt));
-
-		SELECT Id, Email = @salt, NickName = @hash FROM [Users] WHERE  Email LIKE @email
+		SET @hash = HASHBYTES('SHA2_512', CONCAT(@salt, @pwd, @salt));
 		
-		/*
+		
 		SELECT Id, Email, NickName, IsActive
 		FROM Users 
-		WHERE  Email LIKE @email AND Pwd LIKE @hash*/
+		WHERE  Email LIKE @email AND Pwd LIKE @hash
 END
